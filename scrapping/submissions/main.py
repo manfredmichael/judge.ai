bcdfa2260be61501be46f36c8358e5451965f804
manfred michael <koskosanrider@gmail.com>
2022-04-22 08:05:06 +0700

    Use caching when scrapping

--- scrapping/submissions/main.py
+++ scrapping/submissions/main.py
@@ -3,8 +3,20 @@ from pmaw import PushshiftAPI
 
 api = PushshiftAPI()
 
-submissions = api.search_submissions(subreddit="AmItheAsshole",
-        limit=200_000)
+not_finished=True
+while not_finished:
+    try:
+        submissions = api.search_submissions(
+            subreddit="AmItheAsshole",
+            limit=100000,
+            safe_exit=True,
+            before=1650550522,
+            cache_dir='../../data/raw'
+        )
+    except:
+        print('Trying again...')
+        continue
+    not_finished=False
 
 submission_df = pd.DataFrame(submissions)
 submission_df.to_csv('../../data/raw/posts.csv', index=False)
