184debbc274d0f2900bc153afd7147ddeae48ee5
manfred michael <koskosanrider@gmail.com>
2022-04-23 12:19:50 +0700

    Scrape comments

---
+++ scrapping/comments/main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from pmaw import PushshiftAPI
+
+api = PushshiftAPI()
+
+subs_df = pd.read_csv('../../data/raw/posts.csv') 
+sub_ids = list(subs_df.loc[:, 'id']) 
+
+not_finished=True
+while not_finished:
+    try:
+        comment_ids = api.search_submission_comment_ids(
+            ids=sub_ids,
+            safe_exit=True,
+            before=1650550522,
+            cache_dir='../../data/raw',
+        ) 
+        comment_ids = list(comment_ids)
+    except:
+        print('Trying again: get submission')
+        continue
+    not_finished=False
+
+not_finished=True
+while not_finished:
+    try:
+        comments = api.search_comments(
+            ids=comment_ids,
+            safe_exit=True,
+            before=1650550522,
+            cache_dir='../../data/raw',
+        )
+    except:
+        print('Trying again: search comments')
+        continue
+    not_finished=False
+
+
+comments_df = pd.DataFrame(comments)
+comments_df.to_csv('./data/wallstreetbets_comments.csv', header=True, index=False, columns=list(comments_df.axes[1]))
+
